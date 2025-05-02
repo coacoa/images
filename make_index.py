da273e93f769035902bd0c75a47baf207d26bd8b
hoge <tehcnoaka@gmail.com>
2025-05-02 13:43:17 +0900

    a

--- make_index.py
+++ make_index.py
@@ -3,7 +3,8 @@ import os
 output_dir = "out"
 html_path = os.path.join(output_dir, "index.html")
 
-svg_files = [f for f in os.listdir(output_dir) if f.endswith(".svg")]
+# SVGファイル一覧を取得し、名前順（昇順）にソート
+svg_files = sorted(f for f in os.listdir(output_dir) if f.endswith(".svg"))
 
 with open(html_path, "w", encoding="utf-8") as f:
     f.write("<html><head><meta charset='utf-8'><title>SVG一覧</title></head><body>\n")
@@ -11,3 +12,4 @@ with open(html_path, "w", encoding="utf-8") as f:
     for svg in svg_files:
         f.write(f"<li><a href='{svg}' target='_blank'>{svg}</a></li>\n")
     f.write("</ul>\n</body></html>")
+
